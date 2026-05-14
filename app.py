53558d1bccb9872f9676fbb4d077edfdeae950f9
Gongg0910 <chefgonzalonyc@gmail.com>
2026-05-14 19:27:44 +0000

    v4

--- app.py
+++ app.py
@@ -1,79 +1,81 @@
 import sqlite3
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 
 app = Flask(__name__)
 app.secret_key = "dev_secret_key"
 
-# ------------------
-# DATABASE CONNECTION
-# ------------------
+# ------------------ #
+# DATABASE CONNECTION #
+# ------------------ #
 def get_db():
     conn = sqlite3.connect("flask_auth.db")
     conn.row_factory = sqlite3.Row
     return conn
-# ------------------
-# LOADING PAGE
-# ------------------
+
+# ------------------ #
+# LOADING PAGE       #
+# ------------------ #
 @app.route('/')
 def home():
+    if 'username' in session:
+        return redirect(url_for('dashboard'))
     return redirect(url_for('login'))
 
-# ------------------
-# LOGIN ROUTING
-# ------------------
-@app.route('/login', methods =['GET', 'POST'])
+# ------------------ #
+# LOGIN ROUTING      #
+# ------------------ #
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-
+        
         conn = get_db()
         cursor = conn.cursor()
-
-        cursor.execute(""" SELECT * FROM users WHERE email = ? AND password = ? """, (email, password))
+        cursor.execute("SELECT * FROM users WHERE email = ? AND password = ?", (email, password))
         user = cursor.fetchone()
         
         if user:
             session['username'] = user['username']
+            conn.close()
             return redirect(url_for('dashboard'))
         else:
             flash("Invalid email or password")
-        conn.close()
-
+            conn.close()
+            
     return render_template("login.html")
 
-# ------------------
-# DASHBOARD ROUTING
-# ------------------
+# ------------------ #
+# DASHBOARD ROUTING  #
+# ------------------ #
 @app.route('/dashboard')
 def dashboard():
     if 'username' in session:
-        return render_template('dashboard.html', username = session['username'])
+        return render_template('dashboard.html', username=session['username'])
     return redirect(url_for('login'))
 
-# ------------------
-# LOGOUT ROUTING
-# ------------------
+# ------------------ #
+# LOGOUT ROUTING     #
+# ------------------ #
 @app.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('login'))
-# ------------------
-# SIGNUP ROUTING
-# ------------------
-@app.route('/signup', methods =['POST', 'GET'])
+
+# ------------------ #
+# SIGNUP ROUTING     #
+# ------------------ #
+@app.route('/signup', methods=['POST', 'GET'])
 def signup():
     if request.method == 'POST':
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
-
+        
         conn = get_db()
         cursor = conn.cursor()
-
         try:
-            cursor.execute(""" INSERT INTO users (username, email, password) VALUES (?,?,?)""", (username, email, password))
-
+            cursor.execute("INSERT INTO users (username, email, password) VALUES (?,?,?)", (username, email, password))
             conn.commit()
             flash("Account created successfully!")
             return redirect(url_for('login'))
@@ -81,8 +83,169 @@ def signup():
             flash('Email and username already exists!')
         finally:
             conn.close()
-       
     return render_template('signup.html')
+
+
+
+# ------------------------------------------------------------------------#
+# All 3 routes #
+# ------------------------------------------------------------------------#
+@app.route('/todo')
+def todo_list():
+    if 'username' in session:
+        return render_template('index.html', username=session['username'])
+    return redirect(url_for('login'))
+
+@app.route('/blog')
+def blog_1():
+    if 'username' in session:
+        return render_template('blog.html', username=session['username'])
+    return redirect(url_for('login'))
+
+@app.route('/image')
+def image_1():
+    if 'username' in session:
+        return render_template('image.html', username=session['username'])
+    return redirect(url_for('login'))
+# ------------------------------------------------------------------------#
+# All routes #
+# ------------------------------------------------------------------------#
+
+
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------#
+# TO DO LIST TASKS
+# ------------------------------------------------------------------------------------------------------------------------------------------------#
+# ------------------ #
+# GET ALL TASKS      #
+# ------------------ #
+@app.route('/get_tasks', methods=['GET'])
+def get_tasks():
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id FROM users WHERE username = ?", (session['username'],))
+    user = cursor.fetchone()
+
+    cursor.execute("SELECT id, task, completed FROM tasks WHERE user_id = ?", (user['id'],))
+    tasks = cursor.fetchall()
+    conn.close()
+    return jsonify([{"id": row["id"], "task": row["task"], "completed": row["completed"]} for row in tasks])
+
+# ------------------ #
+# ADD NEW TASK       #
+# ------------------ #
+@app.route('/add_task', methods=['POST'])
+def add_task():
+
+    data = request.get_json()
+    task_text = data.get('task')
+    
+    if task_text:
+        conn = get_db()
+        cursor = conn.cursor()
+        
+        cursor.execute("SELECT id FROM users WHERE username = ?", (session['username'],))
+        user = cursor.fetchone()
+
+        if user:
+            cursor.execute("INSERT INTO tasks (user_id, task) VALUES (?, ?)", (user['id'], task_text))
+            conn.commit()
+            conn.close()
+            return jsonify({'status': 'success'})
+            
+        conn.close()
+        
+    return jsonify({'status': 'error'})
+
+# ------------------ #
+# DELETE A TASK      #
+# ------------------ #
+@app.route('/delete_task', methods=['POST'])
+def delete_task():
+
+    data = request.get_json()
+    task_id = data.get('id')
+    
+    conn = get_db()
+    cursor = conn.cursor()
+    
+    cursor.execute("SELECT id FROM users WHERE username = ?", (session['username'],))
+    user = cursor.fetchone()
+    
+    if user:
+        cursor.execute("DELETE FROM tasks WHERE id = ? AND user_id = ?", (task_id, user['id']))
+        conn.commit()
+        
+    conn.close()
+    return jsonify({'status': 'deleted'})
+
+# ------------------ #
+# COMPLETE A TASK    #
+# ------------------ #
+@app.route('/toggle_task', methods=['POST'])
+def toggle_task():
+    data = request.get_json()
+    task_id = data.get('id')
+    completed_status = data.get('completed') 
+    
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id FROM users WHERE username = ?", (session['username'],))
+    user = cursor.fetchone()
+    if user:
+        cursor.execute("UPDATE tasks SET completed = ? WHERE id = ? AND user_id = ?", (completed_status, task_id, user['id']))
+        conn.commit()
+    conn.close()
+    return jsonify({'status': 'updated'})
+
+# ------------------ #
+# UPDATE A TASK      #
+# ------------------ #
+@app.route('/update_task', methods=['POST'])
+def update_task():
+
+    data = request.get_json()
+    task_id = data.get('id')
+    new_text = data.get('task')
+      
+    conn = get_db()
+    cursor = conn.cursor()
+    
+    cursor.execute("SELECT id FROM users WHERE username = ?", (session['username'],))
+    user = cursor.fetchone()
+    
+    if user:
+        cursor.execute("UPDATE tasks SET task = ? WHERE id = ? AND user_id = ?", (new_text.strip(), task_id, user['id']))
+        conn.commit()
+        
+    conn.close()
+    return jsonify({'status': 'updated'})
+# ------------------------------------------------------------------------------------------------------------------------------------------------#
+# TO DO LIST TASKS
+# ------------------------------------------------------------------------------------------------------------------------------------------------#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # ------------------
--- static/script.js
+++ static/script.js
@@ -1,10 +1,144 @@
-function validatePassword(){
-    const password = document.querySelector('input[name="password"]').value
+/* login.html */
+function validatePassword() { 
+    const passwordInput = document.querySelector('input[name="password"]'); 
+    const password = passwordInput.value; 
+    if (password.length < 6) { 
+        alert("Password must be at least 6+ characters"); 
+        return false; 
+    } 
+    return true; 
+} 
 
-    if (password.length < 6){
-        alert("Password must be at least 6+ characters")
-        return false
-    }
+/* -------------------------------------------------------------- index.html -------------------------------------------------------------- */
+/* -------------------------- */
+/* Load task after adding     */
+/* -------------------------- */
+document.addEventListener("DOMContentLoaded", () => { 
 
-    return true
+    if (document.querySelector("#tasklist")) { 
+        loadTasks(); 
+    } 
+    const authForm = document.querySelector('form'); 
+    if (authForm && document.querySelector('input[name="password"]')) { 
+        authForm.onsubmit = validatePassword; 
+    } 
+}); 
+function loadTasks() { 
+    fetch("/get_tasks") 
+        .then(response => response.json()) 
+        .then(data => { 
+            const list = document.querySelector("#tasklist");  
+            list.innerHTML = ""; 
+            data.forEach(task => { 
+                createTaskElement(task.id, task.task, task.completed); 
+            }); 
+        }) 
+        .catch(err => console.error("Error loading tasks:", err)); 
+} 
+
+
+/* -------------------------- */
+/* add task                   */
+/* -------------------------- */
+function addTask() { 
+    const input = document.querySelector('#taskinput'); 
+    const task = input.value.trim(); 
+
+    fetch("/add_task", { 
+        method: "POST", 
+        headers: {"Content-Type": "application/json"}, 
+        body: JSON.stringify({task: task}) 
+    }) 
+    .then(response => response.json()) 
+    .then(() => { 
+        input.value = ""; 
+        loadTasks(); 
+    }) 
+} 
+
+/* -------------------------- */
+/* Delete task                */
+/* -------------------------- */
+function deleteTask(id) { 
+    fetch("/delete_task", { 
+        method: "POST", 
+        headers: {"Content-Type": "application/json"}, 
+        body: JSON.stringify({id: id}) 
+    }) 
+    .then(() => loadTasks()) 
+} 
+
+
+/* ---------------------------------------------------- */
+/* Completion and edit and delete for html's looks*/
+/* ---------------------------------------------------- */
+function createTaskElement(id, taskText, completed){ 
+    const list = document.querySelector('#tasklist'); 
+    const li = document.createElement("li"); 
+    li.classList.add("task-item"); 
+
+    if (completed === 1) { 
+        li.classList.add("completed"); 
+    } 
+
+    // for completion <input>
+    const completion = document.createElement("input"); 
+    completion.type = "checkbox"; 
+    completion.classList.add("task-checkbox"); 
+    completion.checked = (completed === 1); 
+    completion.onchange = () => { 
+        complete_task(id, completion.checked ? 1 : 0); 
+    }; 
+
+
+    // To edit task item (I remember from Et-712 about onclick)
+    const editing = document.createElement("span"); 
+    editing.textContent = taskText; 
+    editing.classList.add("task-text-clickable"); 
+    editing.onclick = () => { 
+        const input = document.createElement("input"); 
+        input.type = "text"; 
+        input.value = editing.textContent.trim(); 
+        input.classList.add("task-edit-item"); 
+
+        input.onblur = () => { 
+            updating_task(id, input.value); 
+        }; 
+
+        li.replaceChild(input, editing); 
+        
+        input.focus(); 
+    }; 
+
+
+    const deleting = document.createElement("button"); 
+    deleting.innerHTML = "❌"; 
+    deleting.onclick = () => deleteTask(id); 
+    deleting.classList.add("btn-delete-item"); 
+
+    /* To run the program, I remember this technique from c++ */
+    li.appendChild(completion); 
+    li.appendChild(editing); 
+    li.appendChild(deleting); 
+    list.appendChild(li); 
+}  
+
+function complete_task(id, value) { 
+    fetch("/toggle_task", { 
+        method: "POST", 
+        headers: {"Content-Type": "application/json"}, 
+        body: JSON.stringify({id: id, completed: value}) 
+    }) 
+    .then(() => loadTasks()) 
+} 
+
+function updating_task(id, new_text) { 
+    if (new_text.trim() === "") return loadTasks(); 
+    fetch("/update_task", { 
+        method: "POST", 
+        headers: {"Content-Type": "application/json"}, 
+        body: JSON.stringify({id: id, task: new_text}) 
+    }) 
+    .then(() => loadTasks()) 
 }
+/* -------------------------------------------------------------- index.html -------------------------------------------------------------- */
